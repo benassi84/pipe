166429c8b05bb76ac419cd0279f25336e2c2b6b1
MacBookDev <macbookdev@MBPdiMacBookDev.station>
2019-03-12 15:11:02 +0100

    up

---
+++ 3.js
@@ -0,0 +1,12 @@
+const through = require('through2');
+
+const write = (buffer, encoding, next) => {
+  this.push(buffer.toString().toUpperCase());
+  next();
+};
+
+const end = (done) => {
+  done();
+}
+
+const stream = through(write, end);
